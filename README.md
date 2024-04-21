efe9c25cf95d3c9bc6f5be8cddb5901312a8e5e0
Bieelogrom <gabrielamorim15436@gmail.com>
2024-04-21 11:02:00 -0300

    Final

---
+++ README.md
@@ -0,0 +1,5 @@
+Projeto desenvolvido para aprendizado de Javascript.
+
+## Instituito Proa - Gabriel Ferreira Amorim
+
+![Logo_Proa](https://www.proa.org.br/wp-content/themes/monsi/assets/img/logo-nova.svg)
--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function trocarCor(){
-    
+function trocarCor() {
+    var cor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"]
+    var cor_final = []
+
+
+    for (var contador = 0; contador < 6; contador++) {
+        var valor = Math.floor(Math.random() * cor.length)
+        cor_final.push(cor[valor])
+    }
+
+    document.getElementsByTagName("body")[0].style.backgroundColor = `#${cor_final.join("")}`
+    document.getElementsByTagName("p")[0].style.color = "#fff"
+    document.getElementsByTagName("span")[0].innerHTML = `#${cor_final.join("")}`
 }
